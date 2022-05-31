f67a3a905e161a8b11af977503eb4ab2052fda87
kimyounsub <tldn0330@naver.com>
2022-05-31 09:03:58 +0900

    아나콘다 파이썬 1

---
+++ 0 학원 아나콘다로 파이썬 수업 정리하기/파이썬 아나콘다 기본기1.py
@@ -0,0 +1,145 @@
+3 + 1
+print("Hello")
+print(3+1)
+
+a = 3
+b = 4
+c = a + b
+a*b
+a/b
+
+a ** b
+
+7 % 3
+3 % 7
+
+7 / 4
+7 // 4
+
+a = 80
+b = 75
+c = 55
+(a+b+c)/3
+
+food = "Python's favorite food is prel"
+say = '"Python is very easy." he says.'
+food1 = 'pyhon\'s favorite food is perl'
+say1 = "\"Python is very easy.\" he says."
+
+multiline = "Life is too short\nYou need python"
+print(multiline)
+multiline1 = '''
+Life is too short
+You need python
+'''
+multiline1
+print(multiline1)
+
+print("\n줄 바꿈\n연습 ")
+print("\t탭 키\t연습")
+print("글자가 \"강조\"되는 효과1")
+print("글자가 \'강조\'되는 효과2")
+
+
+head = "Python"
+tail = " is fun!"
+head + tail
+print(head+tail)
+a = "python "
+a*100
+print("="*50)
+print("My Program")
+print("="*50)
+print("이름 \t나이 \t지역\n오라클 \t10 \t구로구")
+
+#1
+print("이름 \t나이 \t지역")
+print("오라클 \t10 \t구로구")
+print("파이썬 \t3 \t구로고")
+#2
+print("-"*50)
+print("동해무과 백두산이 마르고 닳도록")
+print("-"*50)
+#3
+a = "안녕하세요 "
+print(a*10)
+
+a = "Life is too short, Tou need Python"
+a[-5]
+t = "안\n녕\n하\n세\n요"
+t = "안녕하세요"
+
+# print(t[0]+ \n t[1])
+print(t[1])
+print(t[2])
+print(t[3])
+print(t[4])
+
+print(t[-1])
+print(t[-2])
+print(t[-3])
+print(t[-4])
+print(t[-5])
+b= a[0]+a[1]+a[2]+a[3]
+b
+
+a = "Life is too short, Tou need Python"
+a[0:4]
+a[19:]
+a[:17]
+a[:]
+a[19:-7]
+
+a = "20010331Rainy"
+date = a[:8]
+weather = a[8:]
+date
+weather
+year = a[:4]
+day = a[4:8]
+weather = a[8:]
+year
+day
+weather
+
+a="pithon"
+a[:1]
+a[2:]
+
+a[:1] + 'y' + a[2:]
+
+pin = "881120-1068234"
+yyyymmdd = pin[:6]
+num =pin[7:]
+print(yyyymmdd)
+print(num)
+print(pin[7])
+
+### 
+a = int(input("첫 번째 숫자를 입력하세요:"))
+b = int(input("두 번째 숫자를 입력하세요:"))
+result = a + b
+print(a, "+",b,"=", result)
+result1 = a - b
+print(a, "-",b,"=", result1)
+result2 = a * b
+print(a, "*",b,"=", result2)
+result3 = a / b
+print(a, "/",b,"=", result3)
+
+# 1
+head = "Python은 "
+tail = "아주 훌륭한 프로그래밍 언어이다"
+print("="*50)
+print(head + tail)
+print("="*50)
+
+# 2
+string = "orajava"
+string[0]
+string[:]
+string[3:]
+string[:3]
+string[:4]
+print(string)
+
